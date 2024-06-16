06621b27026c59e70e0c9909dd65c6062531eb3c
Ruslan <dlusipupov612@gmail.com>
2024-06-16 15:30:06 +0500

    Intermediate commit. Fix 4 Task. Add OptionSort.
    Добавленна быстрая сортировка + исправленна ошибка 4 того задания.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -29,8 +29,10 @@ namespace ConsoleApp2
             var CharArr = new List<char>(str.ToCharArray());
             int qua_lit = CharArr.Except(EngLitter).Count();
 
-            if (0 == qua_lit) //возможно подразумевалось try catch
+            if (0 == qua_lit)
+            {
                 rezult = StringRevers(str);
+            }
 
             else
             {
@@ -46,16 +48,40 @@ namespace ConsoleApp2
 
 
 
+
+        public static string OptionSort(string inputString)
+        {
+            Console.WriteLine("\nВыберите тип сортировки:\n1 Быстрая сортировка:" +
+                " Введите 1\n2 Сортировка деревом: Введите 2");
+
+            byte count = Convert.ToByte(Console.ReadLine());
+            string rezult = null;
+
+            if(count == 1)                  //Соритровка дееровм
+            {
+                Console.WriteLine("Сортировка деревом");
+                var arr = inputString.ToCharArray().ToList();
+                arr.Sort();
+                rezult = new string(arr.ToArray());
+            }
+
+
+
+            return rezult;
+        }
+
+
+
+
         public static string StringRevers(string PullString)
         {
             var CharArr = new List<char>(PullString.ToCharArray());
 
-            if (PullString.Length % 2 == 0)
-            {                                               //косоёбит
+            if (PullString.Length % 2 == 0)                     //Чётное количество
+            {                                               
                 string litter_info = String.Empty;
-                /*string modString = String.Empty;*/
+
                 var arr = new List<char>(PullString.ToCharArray().Reverse());
-                var arr2 = new List<char>();
 
                 int len = (PullString.Length / 2);
                 string Furst_part = new string(arr.ToArray()).Substring(len);
@@ -79,17 +105,17 @@ namespace ConsoleApp2
                 for (int i = 0; i < arr.Count; i++)
                 {
 
-                    for (int j = 0, f = 0; j < vowelLetters.Count; j++)    //ddaeddaedd
+                    for (int j = 0; j < vowelLetters.Count; j++)    //asd asd asd asd
                     {                                                      //0123456789
                         if (arr[i] == vowelLetters[j] && haveEl1 == false)
                         {
-                            el1 = i;
-                            haveEl1 = true;
+                                el1 = i;
+                                haveEl1 = true;
                         }
                         if (arr[(arr.Count - 1) - i] == vowelLetters[j] && haveEl2 == false)
                         {
-                            el2 = (arr.Count - 1) - i;
-                            haveEl2 = true;
+                                el2 = (arr.Count-1) - i;
+                                haveEl2 = true;
                         }
 
                         if (haveEl1 == true && haveEl2 == true)
@@ -100,20 +126,26 @@ namespace ConsoleApp2
                         break;
 
                 }
-                    
+
+                string rezSortString = new string(arr.ToArray()).Substring(el1, (el2 - el1)+1);
+                string SortSting = OptionSort(rezSortString);
+
                 foreach (var group in counts)
-                    litter_info = litter_info + $"Символ = {group.Number}, повторяется {group.Count} раз.\n";
+                    litter_info = litter_info + $"Символ = {group.Number}," +
+                        $" повторяется {group.Count} раз.\n";
+
+
 
                 rezult = $"{rezult}\n{litter_info}";
 
-                rezult = $"\n{rezult}\nСтрока из задания 4 = {new string(arr.ToArray()).Substring(el1, el2)}";
+                rezult = $"\n{rezult}\nСтрока из задания 4 = {rezSortString}\nСтрочька из задания 5 = {SortSting}";
 
                 return rezult;
             }
 
+            //--------------------------------------------------------------------------------------------
 
-
-            else
+            else                                           //Не чётное количиство
             {                                              //переварачивет + оирг
                 string litter_info = String.Empty;
                 var arr = new List<char>(PullString.ToCharArray().Reverse());
@@ -137,7 +169,7 @@ namespace ConsoleApp2
                 for (int i = 0; i < arr.Count; i++)
                 {
 
-                    for (int j = 0, f = 0; j < vowelLetters.Count; j++)    //ddaeddaedd
+                    for (int j = 0; j < vowelLetters.Count; j++)    //ddaeddaedd
                     {                                                      //0123456789
                         if (arr[i] == vowelLetters[j] && haveEl1 == false)
                         {
@@ -159,16 +191,23 @@ namespace ConsoleApp2
 
                 }
 
+                //--------------------------------------------------------------------------------
+
+                string rezSortString = new string(arr.ToArray()).Substring(el1, el2 - el1);
+
                 foreach (var group in counts)
                     litter_info = litter_info + $"Символ = {group.Number}, повторяется {group.Count} раз.\n";
 
                 rezult = $"{rezult}\n{litter_info}";
 
-                rezult = $"\n{rezult}\nСтрока из задания 4 = {new string(arr.ToArray()).Substring(el1, el2)}";
+                rezult = $"\n{rezult}\nСтрока из задания 4 = {rezSortString}";
 
                 return rezult;
             }
         }
+
+
+        //-------------------------------------------------------------------------------------------------
 
 
         static void Main(string[] args)
